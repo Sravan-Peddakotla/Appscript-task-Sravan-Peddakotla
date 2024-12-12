d83d7e041758446be8ef8d8e9cbf2098b2bcd6c1
Sravan-Peddakotla <sravan.peddakotla777@gmail.com>
2024-12-12 14:04:13 +0530

    final commit

--- src/components/ProductItem/index.js
+++ src/components/ProductItem/index.js
@@ -2,14 +2,16 @@ import { Component } from "react";
 import './index.css'
 
 class ProductItem extends Component {
+   
     render() {
         const {product, showFilter} = this.props
         const {image, title, category, price} = product
+        const modifiedTitle = title.substring(0, 20) + '...'
         const stylesApply = showFilter ? 'product-item-with-filter' : 'product-item-without-filter'
         return (
             <div className={stylesApply} >
                 <img className="image" src={image} alt={title} />
-                <h4>{title}</h4>
+                <h4>{modifiedTitle}</h4> 
                 <p>{category}</p>
                 <p>Rs. {price}</p>
                 <p>
